14cfb7cce77d296384068285edb73fb812f3cabc
Rakibul Islam Mehedi <rakibulislammehedi4@gmail.com>
2026-06-21 03:38:50 +0600

    test(ledger): cover HelmNextEventCard onTrace visibility and callback

--- test/core/widgets/ledger/helm_next_event_card_test.dart
+++ test/core/widgets/ledger/helm_next_event_card_test.dart
@@ -37,4 +37,39 @@ void main() {
     await tester.pump();
     expect(fired, isTrue);
   });
+
+  testWidgets('View trace button appears when onTrace provided', (tester) async {
+    await tester.pumpWidget(_wrap(HelmNextEventCard(
+      eventLabel: 'NEXT EVENT',
+      eventTitle: 'x',
+      actionLabel: 'Go',
+      onAction: () {},
+      onTrace: () {},
+    )));
+    expect(find.text('View trace'), findsOneWidget);
+  });
+
+  testWidgets('View trace button absent when onTrace null', (tester) async {
+    await tester.pumpWidget(_wrap(HelmNextEventCard(
+      eventLabel: 'NEXT EVENT',
+      eventTitle: 'x',
+      actionLabel: 'Go',
+      onAction: () {},
+    )));
+    expect(find.text('View trace'), findsNothing);
+  });
+
+  testWidgets('tapping View trace fires onTrace', (tester) async {
+    var traceFired = false;
+    await tester.pumpWidget(_wrap(HelmNextEventCard(
+      eventLabel: 'NEXT EVENT',
+      eventTitle: 'x',
+      actionLabel: 'Go',
+      onAction: () {},
+      onTrace: () => traceFired = true,
+    )));
+    await tester.tap(find.text('View trace'));
+    await tester.pump();
+    expect(traceFired, isTrue);
+  });
 }
